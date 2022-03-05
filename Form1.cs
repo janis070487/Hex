6c4d0386aa9a0de6de27bed102281b692b082387
janis070487 <janis070487@gmail.com>
2022-03-05 20:32:31 +0200

    Darbojas divas lapas dizainam

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-
+// Darbojas divas lapas
 //C:\Users\janis\OneDrive\Dators\Projekti\starpkratuve\app.exe
 namespace Hex
 {
